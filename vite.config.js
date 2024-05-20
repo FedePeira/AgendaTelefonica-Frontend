6c30bd8cd75bbeb3df1382cf594d513416725d3d
Federico Peirano <fedepr2345@gmail.com>
2024-05-20 01:35:36 -0300

    Funcionando servidor render en Frontend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://agendatelefonica-wolt.onrender.com',
         changeOrigin: true,
       },
     }
